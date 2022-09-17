c5798c0f3f3c0318c1fbd6f46fd8d63dfc7e5ac0
Lakshmi-1 <54776826+Lakshmi-1@users.noreply.github.com>
2022-09-17 13:09:09 -0500

    Add bottom menu bar

--- snapfit/src/components/MenuBar.js
+++ snapfit/src/components/MenuBar.js
@@ -1,18 +1,33 @@
 import "./MenuBar.css";
 import { Outlet, Link, json } from "react-router-dom";
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import CameraAltIcon from '@mui/icons-material/CameraAlt';
+import CheckroomIcon from '@mui/icons-material/Checkroom';
+import SettingsAccessibilityIcon from '@mui/icons-material/SettingsAccessibility';
 
 export function MenuBar() {
+  const route = window.location.route;
+  const [value, setValue] = React.useState(route);
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
     <div id="container">
-      <Link to="/wardrobe">
-        <button>My Wardrobe</button>
-      </Link>
-      <Link to="/camera">
-        <button>Camera</button>
-      </Link>
-      <Link to="/fits">
-        <button>My Fits</button>
-      </Link>
+      <Box sx={{ width: 500 }}>
+      <BottomNavigation
+        showLabels
+        value={value}
+        onChange={handleChange}
+      >
+        <BottomNavigationAction value='/wardrobe' label="My Wardrobe" icon={<CheckroomIcon />} component={Link} to='/wardrobe'/>
+        <BottomNavigationAction value='/camera' label="Camera" icon={<CameraAltIcon />} component={Link} to='/camera'/>
+        <BottomNavigationAction value='/fits' label="My Fits" icon={<SettingsAccessibilityIcon />} component={Link} to='/fits'/>
+      </BottomNavigation>
+      </Box>
     </div>
   );
 }
