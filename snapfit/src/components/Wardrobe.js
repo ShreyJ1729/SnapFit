9413576a591ebdbf40cd64e9cfafbed847667d6c
Nevin Gilday <nevingilday@gmail.com>
2022-09-17 11:41:35 -0500

    can add pictures from the thing

--- snapfit/src/components/Wardrobe.js
+++ snapfit/src/components/Wardrobe.js
@@ -3,22 +3,27 @@ import { useState, useEffect } from 'react';
 import db from "../firebase";
 
 export function Wardrobe() {
-  let [image, setImage] = useState();
+  let [images, setImage] = useState();
 
   useEffect(() => {
+    let images = [];
     db.collection("shirts").onSnapshot((snapshot) => {
         for(let i = 0; i < snapshot.docs.length; i++){
-          console.log(snapshot.docs[i].data());
           if(snapshot.docs[i].data().photo){
             let a = snapshot.docs[i].data().photo;
-            setImage(a);
+            images.push(a);
           }
         }
+        setImage(images);
       });
+
+      console.log(images);
+      
   }, []);
 
   return (
     <div>
+      {images && images.map(image => <img src={image} style={{width: "100%", height: "auto"}}/>)}
       <div className="scrollmenu">
         <Shirt/><Shirt/><Shirt/><Shirt/><Shirt/><Shirt/><Shirt/><Shirt/>
       </div>
