78416d558c62ea50436b5bdf183bc5670dce8bfe
reese-hoffart <reese.hoffart@gmail.com>
2022-09-17 12:18:22 -0500

    stuff

--- snapfit/src/components/Wardrobe.js
+++ snapfit/src/components/Wardrobe.js
@@ -15,54 +15,20 @@ export function Wardrobe() {
           }
         }
         setImage(images);
-      });
-
-      console.log(images);
-      
+      });      
   }, []);
 
   return (
     <div>
-      {images && images.map(image => <img src={image} style={{width: "100%", height: "auto"}}/>)}
       <div className="scrollmenu">
-        <Shirt/><Shirt/><Shirt/><Shirt/><Shirt/><Shirt/><Shirt/><Shirt/>
+        {images && images.map(image => <img src={image} style={{width: "100%", height: "auto"}}/>)}
       </div>
       <div className="scrollmenu">
-        <Pant/><Pant/><Pant/><Pant/><Pant/><Pant/><Pant/><Pant/>
+        
       </div>
       <div className="scrollmenu">
-        <Shoe/><Shoe/><Shoe/><Shoe/><Shoe/><Shoe/><Shoe/><Shoe/><Shoe/>
+        
       </div>
-    </div>
-  );
-}
-
-function Shirt() {
-  db.collection("shirts").onSnapshot((snapshot) => {
-    snapshot.docs.map((doc) => {
-      let base64Image = doc.data().photo;
-      console.log(base64Image)
-      return (
-        <div>
-          <img src={base64Image}></img>
-        </div>
-      );
-    });
-  });
-}
-
-function Pant() {
-  return (
-    <div class="thisshouldbeaphoto">
-      <p>this is a pant</p>
-    </div>
-  );
-}
-
-function Shoe() {
-  return (
-    <div class="thisshouldbeaphoto">
-      <p>shoo</p>
     </div>
   );
 }
