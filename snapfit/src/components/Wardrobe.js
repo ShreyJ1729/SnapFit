adf4cdd01fbd07801ef65bae643e11e3f00815a3
Lakshmi-1 <54776826+Lakshmi-1@users.noreply.github.com>
2022-09-17 13:42:54 -0500

    Add EditFits.css

--- snapfit/src/components/Wardrobe.js
+++ snapfit/src/components/Wardrobe.js
@@ -1,10 +1,20 @@
 import './Wardrobe.css';
+import * as React from 'react';
+import Chip from '@mui/material/Chip';
+import Stack from '@mui/material/Stack';
 
 export function Wardrobe() {
+  const handleClick = () => {
+    console.info('You clicked the Chip.');
+  };
+
   return (
-    <div>
-      <h1>MY WARDROBE BABY</h1>
-    </div>
+    <Stack direction="row" spacing={1}>
+      <Chip label="Shirts" variant="outlined" onClick={handleClick} />
+      <Chip label="Pants/Shorts" variant="outlined" onClick={handleClick} />
+      <Chip label="Shoes" variant="outlined" onClick={handleClick} />
+    </Stack>
   );
 }
+
 
