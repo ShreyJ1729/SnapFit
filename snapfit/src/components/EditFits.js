febf013a340e323848fe0d69cdb1a3fd1520f78d
Nevin Gilday <nevingilday@gmail.com>
2022-09-17 18:30:15 -0500

    some of pain

--- snapfit/src/components/EditFits.js
+++ snapfit/src/components/EditFits.js
@@ -1,19 +1,26 @@
 import './EditFits.css';
 import { useState, useEffect } from 'react';
 import db from "../firebase";
+import { border } from '@mui/system';
 
-export function EditFits() {
+export function EditFits(props) {
   let [shirtimages, setShirtImage] = useState();
   let [pantimages, setPantImage] = useState();
   let [shoeimages, setShoeImage] = useState();
+
+  let [selectedShirt, setSelectedShirt] = useState(props.currentShirt);
+  let [selectedPants, setSelectedPants] = useState(props.currentPants);
+  let [selectedShoes, setSelectedShoes] = useState(props.currentShoes);
 
   useEffect(() => {
     let shirtimages = [];
     db.collection("shirts").onSnapshot((snapshot) => {
         for(let i = 0; i < snapshot.docs.length; i++){
           if(snapshot.docs[i].data().photo){
-            let a = snapshot.docs[i].data().photo;
-            shirtimages.push(a);
+            shirtimages.push({
+              id: snapshot.docs[i].id,
+              photo: snapshot.docs[i].data().photo
+            });
           }
         }
         setShirtImage(shirtimages);
@@ -22,8 +29,10 @@ export function EditFits() {
       db.collection("pants").onSnapshot((snapshot) => {
         for(let i = 0; i < snapshot.docs.length; i++){
           if(snapshot.docs[i].data().photo){
-            let a = snapshot.docs[i].data().photo;
-            pantimages.push(a);
+            pantimages.push({
+              id: snapshot.docs[i].id,
+              photo: snapshot.docs[i].data().photo
+            });
           }
         }
         setPantImage(pantimages);
@@ -32,31 +41,52 @@ export function EditFits() {
       db.collection("shoes").onSnapshot((snapshot) => {
         for(let i = 0; i < snapshot.docs.length; i++){
           if(snapshot.docs[i].data().photo){
-            let a = snapshot.docs[i].data().photo;
-            shoeimages.push(a);
+            shoeimages.push({
+              id: snapshot.docs[i].id,
+              photo: snapshot.docs[i].data().photo
+            });
           }
         }
         setShoeImage(shoeimages);
       });
   }, []);
 
+  async function confirmNewFit(){
+    //delete old fit from database
+    if(props.fitId != null){
+      await db.collection('fits').doc(props.fitId).delete();
+    }
+    //add new fit into database
+    await db.collection('fits').add({ shirt: selectedShirt, pants: selectedPants, shoes: selectedShoes});
+    props.setFitStatus(false);
+    props.setFitStatus2(false);
+  }
+
+  function goBack(){
+    props.setFitStatus(false);
+    props.setFitStatus2(false);
+  }
+
   return (
     <div className="maincontainer">
       <div className="scrollmenu">
         <div/>
-        {shirtimages && shirtimages.map((image, index) => <img src={image} alt={"shirt"} key={index}/>)}
+        {shirtimages && shirtimages.map((image, index) => <img src={image.photo} alt={"shirt"} key={index} onClick={() => setSelectedShirt(image.id)} style={image.id == selectedShirt ? {borderColor: "black", borderSize: "2px", borderStyle: "solid"}: {}}/>)}
         <div/>
       </div>
       <div className="scrollmenu">
         <div/>
-        {pantimages && pantimages.map((image, index) => <img src={image} alt={"pant"} key={index}/>)}
+        {pantimages && pantimages.map((image, index) => <img src={image.photo} alt={"pant"} key={index} onClick={() => setSelectedPants(image.id)} style={image.id == selectedPants ? {borderColor: "black", borderSize: "2px", borderStyle: "solid"}: {}}/>)}
         <div/>
       </div>
       <div className="scrollmenu">
         <div/>
-        {shoeimages && shoeimages.map((image, index) => <img src={image} alt={"shoe"} key={index}/>)}
+        {shoeimages && shoeimages.map((image, index) => <img src={image.photo} alt={"shoe"} key={index} onClick={() => setSelectedShoes(image.id)} style={image.id == selectedShoes ? {borderColor: "black", borderSize: "2px", borderStyle: "solid"}: {}}/>)}
         <div/>
       </div>
+      <button onClick={goBack}>Go Back</button>
+      <br/>
+      <button onClick={confirmNewFit}>Confirm Fit</button>
       <div style={{height:"50px"}}/>
     </div>
   );
--- snapfit/src/components/Fits.js
+++ snapfit/src/components/Fits.js
@@ -1,7 +1,56 @@
 import EditFits from "./EditFits";
+import {useState, useEffect} from 'react';
+import db from "../firebase";
 
 export function Fits(){
+    const [fits, setFits] = useState();
+    const [currentFit, setCurrentFit] = useState(0);
+
+    const [addingFit, setAddingFit] = useState(false);
+    const [editingFit, setEditingFit] = useState(false);
+
+    useEffect(() => {
+        //get fits
+        let tempFits = [];
+        db.collection("fits").onSnapshot(async (snapshot) => {
+            for(let i = 0; i < snapshot.docs.length; i++){
+                let shirtDoc = await db.collection('shirts').doc(snapshot.docs[i].data().shirt).get();
+                let pantsDoc = await db.collection('pants').doc(snapshot.docs[i].data().pants).get();
+                let shoesDoc = await db.collection('shoes').doc(snapshot.docs[i].data().shoes).get();
+
+                tempFits.push({
+                    shirt: shirtDoc.data().photo,
+                    shirtId:snapshot.docs[i].data().shirt,
+                    pants: pantsDoc.data().photo,
+                    pantsId: snapshot.docs[i].data().pants,
+                    shoes: shoesDoc.data().photo,
+                    shoesId: snapshot.docs[i].data().shoes,
+                    fitId: snapshot.docs[i].id
+                });
+            }
+            setFits(tempFits);
+        });
+    },[addingFit, editingFit]);
+
     return (
-        <EditFits/>
+        <div>
+            {(!(addingFit || editingFit) && ((fits != null) && fits.length > 0)) && 
+            <div style={{display: "flex", alignItems: "center"}}>
+                <button style={{width: "100px", height: "50px"}} onClick={()=>{setCurrentFit(currentFit - 1 < 0 ? currentFit : currentFit-1)}}>last fit</button>
+                <div style={{display: "flex", justifyContent: "center"}} onClick={() => {console.log("i was clicked"); setEditingFit(true); setAddingFit(true);}}>   
+                    <div style={{width: "100%"}}>
+                        {console.log(fits)}
+                        <div><img src={fits[currentFit].shirt}/></div>
+                        <div><img src={fits[currentFit].pants}/></div>
+                        <div><img src={fits[currentFit].shoes}/></div>
+                    </div>
+                </div>  
+                <button style={{width: "100px", height: "50px"}} onClick={()=>{setCurrentFit(currentFit + 1 >= fits.length ? currentFit : currentFit+1)}}>next fit</button>
+            </div>
+            }
+            {(addingFit || editingFit ) && <EditFits setFitStatus={setAddingFit} setFitStatus2={setEditingFit} currentShirt={editingFit && fits != null && fits.length > 0 ? fits[currentFit].shirtId : null} currentPants={editingFit && fits != null && fits.length > 0 ? fits[currentFit].pantsId : null} currentShoes={editingFit && fits != null && fits.length > 0 ? fits[currentFit].shoesId : null} fitId={ fits != null && fits.length > 0 ? fits[currentFit].fitId: null}/>}
+            {!(addingFit || editingFit ) && <button onClick={() => setAddingFit(true)}>Add Fits</button>}
+        </div>
+        
     );
 }
