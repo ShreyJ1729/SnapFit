e5614eab524a0fdced9e6822fe808b7e52eff086
Nevin Gilday <nevingilday@gmail.com>
2022-09-16 23:41:40 -0500

    camera framework

--- snapfit/src/App.js
+++ snapfit/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import {MenuBar} from './components/MenuBar';
+import { Camera } from './components/Camera';
 
 function App() {
   return (
     <div className="App">
+      <Camera/>
       <MenuBar/>
     </div>
   );
