14fe99f30ce5d7e46825ce1391f9f28f17a6653f
Nevin Gilday <nevingilday@gmail.com>
2022-09-17 14:24:37 -0500

    resolved wardrobe merge conflicts

--- snapfit/src/components/Wardrobe.js
+++ snapfit/src/components/Wardrobe.js
@@ -8,10 +8,6 @@ import Box from '@mui/material/Box';
 
 
 export function Wardrobe() {
-  const [value, setValue] = React.useState(0);
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
   const [clothingType, setClothingType] = useState("shirts");
   const [images, setImages] = useState();
 
@@ -29,33 +25,30 @@ export function Wardrobe() {
     });
   }, [clothingType]);
 
-  const handleClick = (value) => {
+  const handleChange = (event, value) => {
+    console.log(value);
     setClothingType(value);
   };
 
 
   return (
-    <Box sx={{ maxWidth: { xs: 320, sm: 480 }, bgcolor: 'background.paper' }}>
-      <Tabs
-        value={value}
-        onChange={handleChange}
-        variant="scrollable"
-        scrollButtons="auto"
-        aria-label="scrollable auto tabs example"
-      >
-        <Tab label="Shirts" />
-        <Tab label="Pants/Shorts" />
-        <Tab label="Shoes" />
-      </Tabs>
-    </Box>
     <div>
-    <Stack id="selectorContainer" direction="row" spacing={1}>
-      <Chip label="Shirts" variant="outlined" value="shirts" onClick={() => handleClick("shirts")} />
-      <Chip label="Pants/Shorts" variant="outlined" value="pants" onClick={() => handleClick("pants")} />
-      <Chip label="Shoes" variant="outlined"value="shoes" onClick={() => handleClick("shoes")} />
-    </Stack>
-    <br/>
-    <br/>
+      <Box sx={{ maxWidth: { xs: 320, sm: 480 }, bgcolor: 'background.paper' }}>
+        <Tabs
+          id="selectorContainer"
+          value={clothingType}
+          onChange={handleChange}
+          variant="scrollable"
+          scrollButtons="auto"
+          aria-label="scrollable auto tabs example"
+        >
+          <Tab label="Shirts" value="shirts"/>
+          <Tab label="Pants/Shorts" value="pants"/>
+          <Tab label="Shoes" value="shoes"/>
+        </Tabs>
+      </Box>
+      <br/>
+      <br/>
       {images && images.map((image,index) => <img src={image} alt={clothingType} key={index}/>)}
     </div>
   );
