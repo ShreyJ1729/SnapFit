a2acc599ca89d91528a47d9112a5905a478a8ba2
reese-hoffart <reese.hoffart@gmail.com>
2022-09-16 23:44:09 -0500

    small

--- snapfit/src/App.js
+++ snapfit/src/App.js
@@ -1,12 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import Wardrobe from './Wardrobe'
+import Wardrobe from './components/Wardrobe'
 import {MenuBar} from './components/MenuBar';
 
 function App() {
   return (
     <div className="App">
-      <h1>camera</h1>
       <MenuBar/>
     </div>
   );
