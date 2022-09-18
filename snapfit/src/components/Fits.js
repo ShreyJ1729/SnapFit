8618c315bb949a03a69a50d969f95bbebb56c3c8
Lakshmi-1 <54776826+Lakshmi-1@users.noreply.github.com>
2022-09-17 19:16:59 -0500

    pretty

--- snapfit/src/components/Fits.js
+++ snapfit/src/components/Fits.js
@@ -1,6 +1,11 @@
 import EditFits from "./EditFits";
 import {useState, useEffect} from 'react';
 import db from "../firebase";
+import * as React from 'react';
+import Fab from '@mui/material/Fab';
+import AddIcon from '@mui/icons-material/Add';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 
 export function Fits(){
     const [fits, setFits] = useState();
@@ -36,7 +41,7 @@ export function Fits(){
         <div>
             {(!(addingFit || editingFit) && ((fits != null) && fits.length > 0)) && 
             <div style={{display: "flex", alignItems: "center"}}>
-                <button style={{width: "100px", height: "50px"}} onClick={()=>{setCurrentFit(currentFit - 1 < 0 ? currentFit : currentFit-1)}}>last fit</button>
+                <Fab color="primary" style={{width: "100px", height: "50px"}} onClick={()=>{setCurrentFit(currentFit - 1 < 0 ? currentFit : currentFit-1)}}><ArrowBackIosIcon /></Fab>
                 <div style={{display: "flex", justifyContent: "center"}} onClick={() => {console.log("i was clicked"); setEditingFit(true); setAddingFit(true);}}>   
                     <div style={{width: "100%"}}>
                         {console.log(fits)}
@@ -45,11 +50,11 @@ export function Fits(){
                         <div><img src={fits[currentFit].shoes}/></div>
                     </div>
                 </div>  
-                <button style={{width: "100px", height: "50px"}} onClick={()=>{setCurrentFit(currentFit + 1 >= fits.length ? currentFit : currentFit+1)}}>next fit</button>
+                <Fab color = "primary" style={{width: "100px", height: "50px"}} onClick={()=>{setCurrentFit(currentFit + 1 >= fits.length ? currentFit : currentFit+1)}}><ArrowForwardIosIcon /></Fab>
             </div>
             }
             {(addingFit || editingFit ) && <EditFits setFitStatus={setAddingFit} setFitStatus2={setEditingFit} currentShirt={editingFit && fits != null && fits.length > 0 ? fits[currentFit].shirtId : null} currentPants={editingFit && fits != null && fits.length > 0 ? fits[currentFit].pantsId : null} currentShoes={editingFit && fits != null && fits.length > 0 ? fits[currentFit].shoesId : null} fitId={ editingFit && fits != null && fits.length > 0 ? fits[currentFit].fitId: null}/>}
-            {!(addingFit || editingFit ) && <button onClick={() => setAddingFit(true)}>Add Fits</button>}
+            {!(addingFit || editingFit ) && <Fab color="primary" onClick={() => setAddingFit(true)}><AddIcon /></Fab>}
         </div>
         
     );
